3b78d2391959245dc492b80d2e073f7dbed9c4a7
Lau, P. H <70358914+JohnnyLPH@users.noreply.github.com>
2022-11-22 16:59:10 +0800

    update skill section

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 console.log(`If you can see this message, JavaScript is working :)`);
 console.log(`Defer is working if '${document.body}' is not NULL!`);
+
+// For Bootstrap Popovers.
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
